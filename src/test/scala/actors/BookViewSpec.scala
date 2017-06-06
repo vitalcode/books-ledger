eb92117f3e70b78c9945fa0b76caaf31ebc59706
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2017-06-06 18:03:22 +0100

    Refactor BookView Spec using EventLogContext

--- src/test/scala/actors/BookViewSpec.scala
+++ src/test/scala/actors/BookViewSpec.scala
@@ -1,63 +1,75 @@
 package actors
 
-import actors.BookActor.BookDebited
+import actors.BookActor.{BookCredited, BookDebited}
 import actors.BookView._
 import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import helpers.DebugTimeout
-import org.scalatest.{Matchers, WordSpecLike}
+import org.scalatest.{FreeSpecLike, Matchers}
 import support.EventLogContext
 
 class BookViewSpec(_system: ActorSystem) extends TestKit(_system)
   with ImplicitSender with EventLogContext with DebugTimeout
-  with WordSpecLike with Matchers {
+  with FreeSpecLike with Matchers {
 
   def this() = this(ActorSystem("BookViewSpec"))
 
   val owner = "TestUser"
   val aggregateId = "Book"
 
-  "A book view" should {
+  "BookView should" - {
+    "add book record and" - {
+      "increase balance when BookActor's `BookDebited` event is emitted" in {
+        withEventLog { (eventLog, emitterId) =>
 
-    "increase balance and add book records when BookActor's `BookDebited` event is emitted" in {
-      withEventLog { (eventLog, emitterId) =>
+          val bookView = system.actorOf(Props(new BookView(emitterId, owner, aggregateId, eventLog)))
 
-        val bookView = system.actorOf(Props(new BookView(emitterId, owner, aggregateId, eventLog)))
+          addEventToLog(bookView, owner, aggregateId,
+            BookDebited(6, "note"),
+            BookDebited(6, "note")
+          )
 
-        addEventToLog(bookView, owner, aggregateId,
-          BookDebited(6, "note"),
-          BookDebited(6, "note")
-        )
+          bookView ! GetBookBalance
 
-        bookView ! GetBookBalance
+          expectMsgPF(timeout) {
+            case BookBalance(amount) => amount shouldBe 12
+            case _ => fail
+          }
 
-        expectMsgPF(timeout) {
-          case BookBalance(amount) => amount shouldBe 12
-          case _ => fail
-        }
+          bookView ! GetBookRecords
 
-        bookView ! GetBookRecords
-
-        expectMsgPF(timeout) {
-          case BookRecords(records) => records shouldBe Seq(6, 6)
-          case _ => fail
+          expectMsgPF(timeout) {
+            case BookRecords(records) => records shouldBe Seq(6, 6)
+            case _ => fail
+          }
         }
       }
+      "decrease balance when BookActor's `BookCredited` event is emitted" in {
+        withEventLog { (eventLog, emitterId) =>
 
+          val bookView = system.actorOf(Props(new BookView(emitterId, owner, aggregateId, eventLog)))
 
-      //    "be able to get book records when called with `GetBookRecords` command" in {
-      //      val owner = "testUser"
-      //      val bookName = UUID.randomUUID().toString
-      //
-      //      val bookView = system.actorOf(Props(BookView(owner, bookName, eventLog)))
-      //
-      //      bookView ! GetBookRecords
-      //
-      //      expectMsgPF(timeout) {
-      //        case BookRecords(records) => records mustBe Seq()
-      //        case _ => fail
-      //      }
-      //    }
+          addEventToLog(bookView, owner, aggregateId,
+            BookDebited(6, "note"),
+            BookDebited(6, "note"),
+            BookCredited(3, "note")
+          )
+
+          bookView ! GetBookBalance
+
+          expectMsgPF(timeout) {
+            case BookBalance(amount) => amount shouldBe 9
+            case _ => fail
+          }
+
+          bookView ! GetBookRecords
+
+          expectMsgPF(timeout) {
+            case BookRecords(records) => records shouldBe Seq(6, 6, 3)
+            case _ => fail
+          }
+        }
+      }
     }
   }
 }
--- src/test/scala/support/EventLogContext.scala
+++ src/test/scala/support/EventLogContext.scala
@@ -1,6 +1,7 @@
 package support
 
 import java.util.UUID
+import java.util.concurrent.atomic.AtomicInteger
 
 import akka.actor.{ActorRef, ActorSystem}
 import akka.testkit.TestProbe
@@ -10,12 +11,14 @@ import com.rbmhtechnology.eventuate.{DurableEvent, VectorTime}
 trait EventLogContext {
 
   val logIdA = "logA"
-  val instanceId = 0
+  var instanceId: Int = _
   var logProbe: TestProbe = _
   var emitterId: String = _
 
+  val instanceIdCounter = new AtomicInteger(0)
 
   def withEventLog(test: (ActorRef, String) => Unit)(implicit system: ActorSystem) = {
+    instanceId = instanceIdCounter.getAndIncrement()
     emitterId = UUID.randomUUID().toString
     logProbe = TestProbe()
     test(logProbe.ref, emitterId)
@@ -24,10 +27,10 @@ trait EventLogContext {
   def addEventToLog(actor: ActorRef, user: String, aggregateId: String, events: Any*) = {
     val userAggregateId = Some(s"$user-$aggregateId")
 
-    logProbe.expectMsg(LoadSnapshot(emitterId, 0))
+    logProbe.expectMsg(LoadSnapshot(emitterId, instanceId))
     logProbe.sender() ! LoadSnapshotSuccess(None, instanceId)
 
-    logProbe.expectMsg(Replay(1, Some(actor), userAggregateId, 0))
+    logProbe.expectMsg(Replay(1, Some(actor), userAggregateId, instanceId))
     val durableEvents = events.map(DurableEvent(_, emitterId, Option(emitterId), Set(), 0L, VectorTime(logIdA -> 1L), logIdA, logIdA, instanceId)).toList
     logProbe.sender() ! ReplaySuccess(durableEvents, durableEvents.size, instanceId)
 
